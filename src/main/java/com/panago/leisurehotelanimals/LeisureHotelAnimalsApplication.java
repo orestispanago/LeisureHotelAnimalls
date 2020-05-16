9c559e8b526f9a77486502d481b63ad52c635b09
orestispanago <orestispanago@gmail.om>
2020-05-16 10:02:12 +0300

    Created springinitializr project

---
+++ src/main/java/com/panago/leisurehotelanimals/LeisureHotelAnimalsApplication.java
@@ -0,0 +1,13 @@
+package com.panago.leisurehotelanimals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeisureHotelAnimalsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeisureHotelAnimalsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/panago/leisurehotelanimals/LeisureHotelAnimalsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.panago.leisurehotelanimals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeisureHotelAnimalsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
